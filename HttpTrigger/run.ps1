f626966aa6f3466a8f38b1182cbcda7be193ea52
Aleksandar Nikolić <alexandair@live.com>
2020-12-08 20:36:19 +0000

    change made in a codespace

--- HttpTrigger/run.ps1
+++ HttpTrigger/run.ps1
@@ -2,7 +2,7 @@ using namespace System.Net
 
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
-
+# change made in a codespace
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
