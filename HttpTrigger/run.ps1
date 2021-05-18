eb32f50e04f85ef14ae727088b21326187196ade
alexandair <alexandair@live.com>
2021-05-19 01:05:43 +0200

    update run.ps1 in VSCode

--- HttpTrigger/run.ps1
+++ HttpTrigger/run.ps1
@@ -2,7 +2,7 @@ using namespace System.Net
 
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
-# change made in a codespace
+# change made in VSCode
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
